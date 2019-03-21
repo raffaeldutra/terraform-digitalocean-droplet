e2140e75e58fe0cbc7394d92338f87de31f704a3
Rafael Dutra <raffaeldutra@gmail.com>
2019-03-21 10:21:31 -0300

    Added size output

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
 output "region" {
+  value       = var.region
   description = "Digital Ocean where your droplet is running in"
-  value = var.region
+}
+
+output "size" {
+  value       = var.size
+  description = "Digital Ocean Droplet size"
 }
