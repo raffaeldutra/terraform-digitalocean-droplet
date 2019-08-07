31d010b8be7784be344a0669e7e6ed8081c98391
Rafael Dutra <raffaeldutra@gmail.com>
2019-08-07 01:56:39 -0300

    Added tags and renamed resource to 'this

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "digitalocean_droplet" "droplet" {
+resource "digitalocean_droplet" "this" {
   name   = "${var.name}-${terraform.workspace}"
   size   = var.size[terraform.workspace]
   image  = var.image[terraform.workspace]
   region = var.region[terraform.workspace]
+  tags   = var.tags
 }
