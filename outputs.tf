b4fe683646fcfe44cb94ecb955560555a3692141
Rafael Dutra <raffaeldutra@gmail.com>
2019-08-07 03:22:23 -0300

    Fixing outputs variables

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "name" {
-  value       = digitalocean_droplet.name
+  value       = digitalocean_droplet.this.name
   description = "Droplet name"
 }
 
 output "region" {
-  value       = digitalocean_droplet.region
+  value       = digitalocean_droplet.this.region
   description = "Digital Ocean where your droplet is running in"
 }
 
 output "size" {
-  value       = digitalocean_droplet.size
+  value       = digitalocean_droplet.this.size
   description = "Digital Ocean Droplet size"
 }
 
 output "image" {
-  value       = digitalocean_droplet.image
+  value       = digitalocean_droplet.this.image
   description = "Digital Ocean image to be used for your Droplet"
 }
 
