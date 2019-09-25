611deff15534059e6b340a26ba76718924bba669
Rafael Dutra <rafael@gandalf.local>
2019-09-25 00:35:29 -0300

    Added default environment

--- variables.tf
+++ variables.tf
@@ -8,9 +8,10 @@ variable "size" {
   type        = map
   description = "(Required) The unique slug that indentifies the type of Droplet"
   default = {
-    dev = "512mb"
-    stg = "512mb"
-    prd = "512mb"
+    default = "512mb"
+    dev     = "512mb"
+    stg     = "512mb"
+    prd     = "512mb"
   }
 }
 
@@ -18,9 +19,10 @@ variable "image" {
   type        = map
   description = "(Required) The Droplet image ID or slug"
   default = {
-    dev = "ubuntu-18-04-x64"
-    stg = "ubuntu-18-04-x64"
-    prd = "ubuntu-18-04-x64"
+    default = "ubuntu-18-04-x64"
+    dev     = "ubuntu-18-04-x64"
+    stg     = "ubuntu-18-04-x64"
+    prd     = "ubuntu-18-04-x64"
   }
 }
 
@@ -28,9 +30,10 @@ variable "region" {
   type        = map
   description = "(Required) The region to create your droplets"
   default = {
-    dev = "nyc3"
-    stg = "nyc3"
-    prd = "nyc3"
+    default = "nyc3"
+    dev     = "nyc3"
+    stg     = "nyc3"
+    prd     = "nyc3"
   }
 }
 
